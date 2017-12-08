fd1dea852e53bce3ac348b11845022b3d4417068
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2017-12-08 16:27:50 +0100

    Something added, be more specific next time

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "dinosaurs#index"
   get "dinosaurs" => "dinosaurs#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
